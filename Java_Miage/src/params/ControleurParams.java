ada25f5fae2d3567e5888c42d5820a634a2cd59f
Quentin <quentin.beaussart@gmail.com>
2019-01-22 09:39:48 +0100

    Récupération des chemins des fichiers importés

--- Java_Miage/src/params/ControleurParams.java
+++ Java_Miage/src/params/ControleurParams.java
@@ -10,6 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import main.MainApp;
@@ -26,6 +27,12 @@ public class ControleurParams {
 
 	@FXML
 	private Button boutonOk;
+	
+	@FXML
+	private Label pathElement;
+	
+	@FXML 
+	private Label pathChaines;
 
 	private MainApp mainApp;
 	private File fichier;
@@ -35,11 +42,25 @@ public class ControleurParams {
 	}
 	
 	@FXML
-	private void clicBoutonImport() {
+	private void clicBoutonImportElem() {
 		Stage fenetre = new Stage();
 		FileChooser explorateur = new FileChooser();
 		explorateur.setTitle("Explorateur");
 		this.fichier = explorateur.showOpenDialog(fenetre);
+		String filePathElement = fichier.getAbsolutePath();
+		System.out.println(filePathElement);
+		this.pathElement.setText(filePathElement);
+	}
+	
+	@FXML
+	private void clicBoutonImportChaine() {
+		Stage fenetre = new Stage();
+		FileChooser explorateur = new FileChooser();
+		explorateur.setTitle("Explorateur");
+		this.fichier = explorateur.showOpenDialog(fenetre);
+		String filePathChaine = fichier.getAbsolutePath();
+		System.out.println(filePathChaine);
+		this.pathChaines.setText(filePathChaine);
 	}
 	
 	@FXML
