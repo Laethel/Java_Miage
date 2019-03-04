9006d14a9dd9d67e8085eeafda34bc755e29c867
RIQUART Damian <damian.riquart@hotmail.com>
2019-03-04 09:29:45 +0100

    Début ihm

---
+++ Java_Miage/src/dao/ChaineDAO.java
@@ -0,0 +1,29 @@
+package dao;
+
+import java.util.ArrayList;
+
+import modele.Chaine;
+
+public class ChaineDAO extends Dao {
+
+	public boolean create(Object obj) {
+		return false;
+	}
+
+	public boolean delete(Object obj) {
+		return false;
+	}
+
+	public boolean update(Object oldObj, Object newObj) {
+		return false;
+	}
+
+	public Object find(int id) {
+		return null;
+	}
+
+	public ArrayList<Chaine> findAll() {
+		return null;
+	}
+
+}
